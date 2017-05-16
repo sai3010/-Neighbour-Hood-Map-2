8eb4722724ad08965afb8535a23b859e381bc0bb
sai3010 <saiprasanth2007@gmail.com>
2017-05-16 15:47:05 +0530

    marker anim

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
 var map;
 
 function initialize() {
-    var mapOptions = {
-        zoom: 14,
-        center: new google.maps.LatLng(12.9154502, 77.4981244),
-        disableDefaultUI: true
-    };
+        var mapOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(12.9154502, 77.4981244),
+            disableDefaultUI: true
+        };
 
-    map = new google.maps.Map(document.getElementById('gmap'),
-        mapOptions);
-locationsModel.google(!!window.google);
-}
-// location function
+        map = new google.maps.Map(document.getElementById('gmap'),
+            mapOptions);
+        locationsModel.google(!!window.google);
+    }
+    // location function
 function Location(title, lng, lat, venueId) {
     var self = this;
     this.title = title;
@@ -40,58 +40,59 @@ function Location(title, lng, lat, venueId) {
         });
     }();
 
-this.addMapFunctionality = ko.computed(function() {
-    if (locationsModel.google()) {
-    self.infowindow = new google.maps.InfoWindow();
+    this.addMapFunctionality = ko.computed(function() {
+        if (locationsModel.google()) {
+            self.infowindow = new google.maps.InfoWindow();
 
-    // Assigns a marker icon.
-    self.icon = 'http://www.googlemapsmarkers.com/v1/990000/';
-    self.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(self.lng, self.lat),
-        animation: google.maps.Animation.DROP,
-        map: map,
-        title: self.title,
-        icon: self.icon
-    });
+            // Assigns a marker icon.
+            self.icon = 'http://www.googlemapsmarkers.com/v1/990000/';
+            self.marker = new google.maps.Marker({
+                position: new google.maps.LatLng(self.lng, self.lat),
+                animation: google.maps.Animation.DROP,
+                map: map,
+                title: self.title,
+                icon: self.icon
+            });
 
-    // Assigns a click event listener to the marker to open the info window.
-    self.addListener = google.maps.event.addListener(self.marker, 'click', function() {
- for (var i = 0; i < locationsModel.locations.length; i++) {
-            locationsModel.locations[i].infowindow.close();
+            // Assigns a click event listener to the marker to open the info window.
+            self.addListener = google.maps.event.addListener(self.marker, 'click', function() {
+                for (var i = 0; i < locationsModel.locations.length; i++) {
+                    locationsModel.locations[i].infowindow.close();
+                }
+                toggleBounce(self.marker);
+                map.panTo(self.marker.getPosition())
+                self.infowindow.setContent(self.content);
+                self.infowindow.open(map, self.marker);
+            });
         }
-       //toggleBounce(self.marker);
-       map.panTo(self.marker.getPosition())
-        self.infowindow.setContent(self.content);
-        self.infowindow.open(map, self.marker);
-});
-}
-});
+    });
 };
-function toogleBounce(marker){
-	if(marker.getAnimation()!==null){
-		marker.setAnimation(null);
-	}else{
-		marker.setAnimation(google.maps.Animation.Bounce);
-		setTimeout(function(){
-			marker.setAnimation(null);
-		},1000);
-	}
+
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.Bounce);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 10000);
+    }
 }
 
 // Contains all the locations and search function.
 var locationsModel = {};
-locationsModel.google=ko.observable(!!window.google);//false
-    locationsModel.locations= ko.observableArray([
-        new Location('By 2 coffee', 12.9648003, 77.5389259, '51d8034a498e44075a4a92fc'),
-        new Location('Captain\'s Food Court', 12.9190615, 77.5183429, '52b4215211d2e0e5ef99e09b'),
-        new Location('Cafe Coffee Day', 12.9190613, 77.5117768, '4c61410f924b76b0ae8afab9'),
-        new Location('Gopalan Arcade', 12.93597, 77.5155649, '4beade509fa3ef3ba52f80c9'),
-        new Location('Kaapi Katte', 12.9164329, 77.5184768, '4f140fece4b0253d4f512942'),
-        new Location('Chung Wah', 12.9250658, 77.5486708, '4bd2c60cb221c9b62145d8d0'),
-        new Location('Royal Andhra Spice', 12.9057565, 77.5188221, '4de4d44ec65b7a3e21522847'),
-        new Location('Pizza Hut', 12.9057559, 77.5035012, '4e7a0e46aeb79f7dabc48535')
-    ]);
-    locationsModel.query= ko.observable('');
+locationsModel.google = ko.observable(!!window.google); //false
+locationsModel.locations = ko.observableArray([
+    new Location('By 2 coffee', 12.9648003, 77.5389259, '51d8034a498e44075a4a92fc'),
+    new Location('Captain\'s Food Court', 12.9190615, 77.5183429, '52b4215211d2e0e5ef99e09b'),
+    new Location('Cafe Coffee Day', 12.9190613, 77.5117768, '4c61410f924b76b0ae8afab9'),
+    new Location('Gopalan Arcade', 12.93597, 77.5155649, '4beade509fa3ef3ba52f80c9'),
+    new Location('Kaapi Katte', 12.9164329, 77.5184768, '4f140fece4b0253d4f512942'),
+    new Location('Chung Wah', 12.9250658, 77.5486708, '4bd2c60cb221c9b62145d8d0'),
+    new Location('Royal Andhra Spice', 12.9057565, 77.5188221, '4de4d44ec65b7a3e21522847'),
+    new Location('Pizza Hut', 12.9057559, 77.5035012, '4e7a0e46aeb79f7dabc48535')
+]);
+locationsModel.query = ko.observable('');
 
 
 // Search function for filtering through the list of locations based on the name of the location.
@@ -101,18 +102,19 @@ locationsModel.search = ko.computed(function() {
 
     return ko.utils.arrayFilter(self.locations(), function(location) {
         var matched = location.title.toLowerCase().indexOf(query) != -1;
-        if(location.marker){
-        location.marker.setVisible(matched);
-    }
+        if (location.marker) {
+            location.marker.setVisible(matched);
+        }
         return matched;
     });
 }, locationsModel);
 // Opens the info window for the location marker.
-    locationsModel.openInfowindow = function(location) {
-       google.maps.event.trigger(location.marker, "click");
-    };
-function googleError(){
-	alert("google Error");
+locationsModel.openInfowindow = function(location) {
+    google.maps.event.trigger(location.marker, "click");
+};
+
+function googleError() {
+    alert("google Error");
 }
 
 ko.applyBindings(locationsModel);
